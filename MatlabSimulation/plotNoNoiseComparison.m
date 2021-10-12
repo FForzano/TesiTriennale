3853eb4e5f6b7e7b9a47223f6a0a5ab68b94e362
FForzano <f.forzano99@gmail.com>
2021-10-12 22:52:55 +0200

    end of day

--- MatlabSimulation/plotNoNoiseComparison.m
+++ MatlabSimulation/plotNoNoiseComparison.m
@@ -9,20 +9,20 @@ function plotNoNoiseComparison(N,p0,p1,n_th)
     
     hold on;
     index = 0;
-    for k = [2]
+    for k = [1]
         index = 1;
         Pe_noisy = [];
         Pe_noNoisy = [];
 
         % Pe = NaN per indici minori di k
         if k ~= 0
-            for j = [1:0.02:k]
+            for j = [0:0.02:k]
                 %Pe_noisy(index) = NaN;
                 Pe_noNoisy(index) = NaN;
                 index = index+1;
             end
         end
-        
+        Pe_noNoisy(1)=1;
         for np = [k:0.02:10]
             mu = np2mu(np,setNoisyPACS(0,k,n_th),0,5,delta,'nPACS');
             Xi0 = setNoisyPACS(-mu,k,n_th);
@@ -35,7 +35,7 @@ function plotNoNoiseComparison(N,p0,p1,n_th)
    
         %plot([0:100],Pe_noisy,strcat(colors(k+1),'--',symbols(k+1)));
         length(Pe_noNoisy)
-        plot([0:0.02:9.02],Pe_noNoisy,strcat('r','-'));
+        plot([0:0.02:10.02],Pe_noNoisy,strcat('r','-'));
     end
     
         xlabel('$n_p$','Interpreter','Latex')
